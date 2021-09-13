9e40616801948091e3596f3080f928cbaa93cea0
Mancefas <mantvydasliobe@gmail.com>
2021-09-13 15:06:54 +0300

    mic on click - hear my name added

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const allSections = document.querySelectorAll('.section');
-const revealSection = function(entries, observer) {
-    const [entry] = entries;
-    if(!entry.isIntersecting) return;
-    entry.target.classList.remove('section-hidden')
-    observer.unobserve(entry.target)
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
 };
 const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.25,
+  root: null,
+  threshold: 0.25,
 });
 
-allSections.forEach(section => {
-    sectionObserver.observe(section);
-    section.classList.add('section-hidden')
-})
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
+
+const micBtn = document.getElementById("microphone");
+const audio = document.getElementById("audio");
+micBtn.addEventListener("click", () => {
+  audio.play();
+});
