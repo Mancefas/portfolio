de7789bbee7dd7fc0282aa5a7d1235cc8537e81b
Mancefas <mantvydasliobe@gmail.com>
2021-12-18 20:04:27 +0200

    Css changes on landing page

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -6,30 +6,33 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const Landing = () => {
   return (
-    <Container maxWidth="large" sx={{ margin: "auto" }}>
-      <Box
-        sx={{
-          minHeight: "80vh",
-          width: "95vw",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <Typography variant="h3">
-          Mantvydas made websites and work samples
-        </Typography>
-        <Typography variant="h5">
-          Front end website developer based in EU
-        </Typography>
-        <Box>
-          <DevicesIcon fontSize="large" color="success" />
-          <MouseIcon fontSize="medium" color="success" />
-        </Box>
+    <Container
+      className="container-background"
+      maxWidth="large"
+      sx={{
+        margin: "auto",
+        height: "80vh",
+        width: "95vw",
+        marginTop: "2vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        textAlign: "center",
+        borderRadius: "9px",
+      }}
+    >
+      <Typography variant="h3">
+        Mantvydas made websites and work samples
+      </Typography>
+      <Typography variant="h5">
+        Front end website developer based in EU
+      </Typography>
+      <Box>
+        <DevicesIcon fontSize="large" color="success" />
+        <MouseIcon fontSize="medium" color="success" />
       </Box>
-      <Container maxWidth="large" sx={{ margin: "auto", textAlign: "center" }}>
+      <Container maxWidth="large" sx={{ margin: "0", textAlign: "center" }}>
         <MailOutlineIcon sx={{ color: "#D2691E" }} />
         <Typography>mantvydasdev@gmail.com</Typography>
       </Container>
