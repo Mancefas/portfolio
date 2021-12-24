ea9b80b23081e1e9b1e1278a33f32a0efafcb805
Mancefas <mantvydasliobe@gmail.com>
2021-12-24 13:38:08 +0200

    added : light/dark theme

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,130 +1,144 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Typography, Grid, Box, Link } from "@mui/material";
+import { Typography, Grid, Box, Link, Button } from "@mui/material";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <Grid
-      container
-      sx={{
-        minHeight: "12vh",
-        height: "fit-content",
-        width: "100%",
-        borderRadius: "9px",
-        margin: 0,
-        borderColor: "#fff",
-      }}
-      borderBottom={1}
-    >
-      <Grid
-        item
-        xs={12}
-        md={4}
-        sx={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          marginTop: "1rem",
-          gap: "1rem",
-        }}
+    <>
+      <Box
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
       >
-        <NavLink
-          style={({ isActive }) => {
-            return {
-              display: "block",
-              color: "#333d51",
-              backgroundColor: isActive ? "" : "#ffffffce",
-              padding: "0.3rem 0.5rem",
-              borderRadius: "10PX",
-              textDecoration: "none",
-            };
-          }}
-          to={`/react-websites`}
-        >
-          React
-        </NavLink>
-        <NavLink
-          style={({ isActive }) => {
-            return {
-              display: "block",
-              color: "#333d51",
-              backgroundColor: isActive ? "" : "#ffffffce",
-              padding: "0.3rem 0.5rem",
-              borderRadius: "10PX",
-              textDecoration: "none",
-            };
-          }}
-          to={`/html-websites`}
-        >
-          Html
-        </NavLink>
-        <NavLink
-          style={({ isActive }) => {
-            return {
-              display: "block",
-              color: "#333d51",
-              backgroundColor: isActive ? "" : "#ffffffce",
-              padding: "0.3rem 0.5rem",
-              borderRadius: "10PX",
-              textDecoration: "none",
-            };
-          }}
-          to={`/wordpress-websites`}
-        >
-          Wordpress
-        </NavLink>
-      </Grid>
+        <Button onClick={props.themeColorHandler}>
+          {props.darkTheme ? <LightModeIcon /> : <DarkModeIcon />}
+        </Button>
+      </Box>
       <Grid
-        item
-        xs={12}
-        md={4}
+        container
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "1rem",
+          minHeight: "12vh",
+          height: "fit-content",
+          width: "100%",
+          borderRadius: "9px",
+          margin: 0,
+          borderColor: "#fff",
         }}
+        borderBottom={1}
       >
-        <NavLink
-          to={"/"}
-          style={{
-            textDecoration: "none",
-            color: "#333d51",
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            marginTop: "1rem",
+            gap: "1rem",
           }}
         >
-          <Box sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
-            <Typography variant="h3">Mantvydas </Typography>
-            <BusinessCenterIcon fontSize="large" />
-          </Box>
-        </NavLink>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        md={4}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link
-          href="mailto:mantvydasdev@gmail.com"
-          underline="none"
+          <NavLink
+            className={"text-primary"}
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                backgroundColor: isActive ? "" : "#ffffffce",
+                color: isActive ? "" : "black",
+                padding: "0.3rem 0.5rem",
+                borderRadius: "10PX",
+                textDecoration: "none",
+              };
+            }}
+            to={`/react-websites`}
+          >
+            React
+          </NavLink>
+          <NavLink
+            className={"text-primary"}
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                backgroundColor: isActive ? "" : "#ffffffce",
+                color: isActive ? "" : "black",
+                padding: "0.3rem 0.5rem",
+                borderRadius: "10PX",
+                textDecoration: "none",
+              };
+            }}
+            to={`/html-websites`}
+          >
+            Html
+          </NavLink>
+          <NavLink
+            className={"text-primary"}
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                backgroundColor: isActive ? "" : "#ffffffce",
+                color: isActive ? "" : "black",
+                padding: "0.3rem 0.5rem",
+                borderRadius: "10PX",
+                textDecoration: "none",
+              };
+            }}
+            to={`/wordpress-websites`}
+          >
+            Wordpress
+          </NavLink>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={4}
           sx={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            color: "#333d51",
+            gap: "1rem",
           }}
         >
-          <MailOutlineIcon sx={{ color: "#ffffffce" }} />
-          mantvydasdev@gmail.com
-        </Link>
+          <NavLink
+            to={"/"}
+            className={"text-primary"}
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+              <Typography variant="h3">Mantvydas </Typography>
+              <BusinessCenterIcon fontSize="large" />
+            </Box>
+          </NavLink>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Link
+            href="mailto:mantvydasdev@gmail.com"
+            underline="none"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#333d51",
+            }}
+          >
+            <MailOutlineIcon sx={{ color: "#ffffffce" }} />
+            mantvydasdev@gmail.com
+          </Link>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
