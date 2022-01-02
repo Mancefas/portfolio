ed48c03151c3c10dc6137da69a8ff89564e7bf09
Mancefas <mantvydasliobe@gmail.com>
2022-01-02 10:58:47 +0200

    added component to show 3 plans to choose from

---
+++ src/Components/Plans.js
@@ -0,0 +1,190 @@
+import React from "react";
+import { Container, Grid, Typography, Paper, Button, Box } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3Alt,
+  faBootstrap,
+  faReact,
+  faMaxcdn,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faBookReader,
+  faQuestionCircle,
+  faWindowClose,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Link } from "react-router-dom";
+
+const Plans = () => {
+  return (
+    <Container
+      sx={{
+        textAlign: "center",
+        minHeight: "85vh",
+        height: "fit-content",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "space-around",
+        justifyContent: "space-around",
+        marginTop: "2rem",
+        marginBottom: "2rem",
+      }}
+      className="text-primary"
+    >
+      <Typography variant="h3" sx={{ marginBottom: "1rem" }}>
+        What can I do for you <FontAwesomeIcon icon={faQuestionCircle} />
+      </Typography>
+      <Grid container gap={1} sx={{ justifyContent: "center" }}>
+        <Grid item xs={12} md={3}>
+          <Container
+            sx={{
+              minHeight: "50vh",
+              height: "fit-content",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-around",
+              backgroundColor: "#ffffff9c",
+              borderRadius: "9px",
+              border: "3px",
+            }}
+            className="shadow-for-box container-background"
+          >
+            <Typography variant="h5">Static website</Typography>
+            <Typography>Static web pages for you bussiness. </Typography>
+            <Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faHtml5}
+                  color="red"
+                />
+                <Typography>HTML</Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faCss3Alt}
+                  color="red"
+                />
+                <Typography>CSS</Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faBootstrap}
+                  color="red"
+                />
+                <Typography>Bootstrap</Typography>
+              </Box>
+            </Box>
+
+            <Link to={`/html-websites`} style={{ textDecoration: "none" }}>
+              <Button variant="contained" sx={{ margin: "1rem" }}>
+                Samples
+              </Button>
+            </Link>
+          </Container>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Container
+            sx={{
+              minHeight: "50vh",
+              height: "fit-content",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-around",
+              backgroundColor: "#ffffff9c",
+              borderRadius: "9px",
+              border: "3px",
+            }}
+            className="shadow-for-box container-background"
+          >
+            <Typography variant="h5">Dynamic website</Typography>
+            <Typography>
+              Dynamic website or web app that reacts to user input.{" "}
+            </Typography>
+
+            <Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faReact}
+                  color="red"
+                />
+                <Typography>React.js</Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faCss3Alt}
+                  color="red"
+                />
+                <Typography>CSS</Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <FontAwesomeIcon
+                  size="2x"
+                  className="link-color"
+                  icon={faMaxcdn}
+                  color="red"
+                />
+                <Typography>Material UI</Typography>
+              </Box>
+            </Box>
+            <Link to={`/react-websites`} style={{ textDecoration: "none" }}>
+              <Button variant="contained" sx={{ margin: "1rem" }}>
+                Samples
+              </Button>
+            </Link>
+          </Container>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Container
+            sx={{
+              minHeight: "50vh",
+              height: "fit-content",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-around",
+              backgroundColor: "#ffffff9c",
+              borderRadius: "9px",
+              border: "3px",
+            }}
+            className="shadow-for-box container-background"
+          >
+            <Typography variant="h5">Full stack website</Typography>
+            <Typography>
+              Dynamic website that reacts to user input, has its own API{" "}
+            </Typography>
+            <Box>
+              <FontAwesomeIcon
+                className="link-color"
+                style={{ margin: "1rem" }}
+                icon={faWindowClose}
+                size="2x"
+              />
+              <FontAwesomeIcon
+                className="link-color"
+                style={{ margin: "1rem" }}
+                icon={faBookReader}
+                size="2x"
+              />
+            </Box>
+            <Typography variant="h6">Still working on it</Typography>
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Plans;
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -16,77 +16,82 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 import ContainerBox from "../Components/ContainerBox";
+import Plans from "../Components/Plans";
 
 const Landing = () => {
   return (
-    <ContainerBox>
-      <Typography variant="h3">Hello and welcome !</Typography>
-      <Typography variant="h5">
-        I am Front-end developer from EU. I can build you a fast and stable
-        website in a manageable time frame. Specializing in react.js, but like
-        to code in HTML,CSS, Bootstrap as well.
-      </Typography>
-      <Typography variant="h5">
-        I can build websites SEO optimized and content optimized(for speed). And
-        all websites I build are mobile-friendly (responsive).
-      </Typography>
+    <>
+      <ContainerBox>
+        <Typography variant="h3">Hello and welcome !</Typography>
+        <Typography variant="h5">
+          I am Front-end developer from EU. I can build you a fast and stable
+          website in a manageable time frame. Specializing in react.js, but like
+          to code in HTML,CSS, Bootstrap as well.
+        </Typography>
+        <Typography variant="h5">
+          I can build websites SEO optimized and content optimized(for speed).
+          And all websites I build are mobile-friendly (responsive).
+        </Typography>
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "1rem",
-        }}
-      >
-        <CodeIcon fontSize="large" color="primary" />
-        <Typography variant="h4">Coding languages</Typography>
-        <CodeOffIcon fontSize="large" color="primary" />
-      </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <CodeIcon fontSize="large" color="primary" />
+          <Typography variant="h4">Coding languages</Typography>
+          <CodeOffIcon fontSize="large" color="primary" />
+        </Box>
 
-      <Grid container spacing={1} sx={{ justifyContent: "space-evenly" }}>
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faHtml5} size="3x" color="red" />
-          <Typography>HTML</Typography>
+        <Grid container spacing={1} sx={{ justifyContent: "space-evenly" }}>
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faHtml5} size="3x" color="red" />
+            <Typography>HTML</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faCss3} size="3x" color="blue" />
+            <Typography>CSS</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faJs} size="3x" color="#F7DF1E" />
+            <Typography>JavaScript</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faSass} size="3x" color="#CE679A" />
+            <Typography>SASS</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faReact} size="3x" color="#61DAFB" />
+            <Typography>React</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faBootstrap} size="3x" color="#7410F0" />
+            <Typography>Bootstrap</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faMaxcdn} size="3x" color="#007FFF" />
+            <Typography>Material UI</Typography>
+          </Grid>
+
+          <Grid item xs={4} md={1}>
+            <FontAwesomeIcon icon={faFontAwesome} size="3x" color="#007FFF" />
+            <Typography>Font awesome</Typography>
+          </Grid>
         </Grid>
+      </ContainerBox>
 
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faCss3} size="3x" color="blue" />
-          <Typography>CSS</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faJs} size="3x" color="#F7DF1E" />
-          <Typography>JavaScript</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faSass} size="3x" color="#CE679A" />
-          <Typography>SASS</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faReact} size="3x" color="#61DAFB" />
-          <Typography>React</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faBootstrap} size="3x" color="#7410F0" />
-          <Typography>Bootstrap</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faMaxcdn} size="3x" color="#007FFF" />
-          <Typography>Material UI</Typography>
-        </Grid>
-
-        <Grid item xs={4} md={1}>
-          <FontAwesomeIcon icon={faFontAwesome} size="3x" color="#007FFF" />
-          <Typography>Font awesome</Typography>
-        </Grid>
-      </Grid>
-    </ContainerBox>
+      <Plans />
+    </>
   );
 };
 
