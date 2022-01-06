3b5004912c84403bd75ed154550ea3f0e33e9b07
Mancefas <mantvydasliobe@gmail.com>
2022-01-06 14:58:42 +0200

    Added: contact form with emailjs email sending

---
+++ src/Components/Contact.js
@@ -0,0 +1,138 @@
+import React, { useState, useRef } from "react";
+import {
+  Container,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Modal,
+} from "@mui/material";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import config from "../config.json";
+
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const [open, setOpen] = useState(false);
+  const form = useRef();
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        config.CONTACT_SERVICE_ID,
+        config.CONTACT_TEMPLATE_ID,
+        form.current,
+        config.CONTACT_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setOpen(false);
+  };
+
+  return (
+    <Container
+      sx={{
+        minHeight: "10vh",
+        marginBottom: "2rem",
+        marginBottom: "2rem",
+        padding: "1rem",
+        width: "fit-content",
+      }}
+    >
+      <Box sx={{ display: "flex", justifyContent: "center", margin: "1rem" }}>
+        <Typography variant="h4">You can contact me </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2rem",
+        }}
+      >
+        <Button variant="outlined" onClick={handleOpen} className="link-color">
+          Contact Form
+        </Button>
+        <Button
+          href="mailto:mantvydasdev@gmail.com"
+          variant="outlined"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="link-color"
+        >
+          <MailOutlineIcon />
+          Write email
+        </Button>
+      </Box>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{ marginTop: "15vh" }}
+      >
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          style={{ minWidth: "30vw", width: "fit-content", margin: "auto" }}
+          className="container-background"
+        >
+          <Container
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              padding: "1rem",
+              borderRadius: "9px",
+            }}
+          >
+            <Typography variant="h5" sx={{ fontFamily: "Lora" }}>
+              Contact me
+            </Typography>
+
+            <TextField
+              label={"Your Name"}
+              margin="normal"
+              name="userName"
+              required
+            ></TextField>
+            <TextField
+              label={"Your email"}
+              margin="normal"
+              name="userEmail"
+              type="email"
+              required
+            ></TextField>
+            <TextField
+              label={"Your question or suggestion "}
+              margin="normal"
+              multiline
+              name="message"
+              required
+            ></TextField>
+            <Button variant="contained" type="submit">
+              Send
+            </Button>
+          </Container>
+        </form>
+      </Modal>
+    </Container>
+  );
+};
+
+export default Contact;
--- src/Components/Plans.js
+++ src/Components/Plans.js
@@ -11,28 +11,72 @@ import {
 import { Link } from "react-router-dom";
 
 import dataPlans from "../store/dataPlans";
+import Contact from "./Contact";
 
 const Plans = () => {
   return (
-    <Container
-      sx={{
-        textAlign: "center",
-        minHeight: "85vh",
-        height: "fit-content",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "space-around",
-        justifyContent: "space-around",
-        marginTop: "2rem",
-        marginBottom: "2rem",
-      }}
-      className="text-primary"
-    >
-      <Typography variant="h3" sx={{ marginBottom: "1rem" }}>
-        What can I do for you <FontAwesomeIcon icon={faQuestionCircle} />
-      </Typography>
-      <Grid container gap={1} sx={{ justifyContent: "center" }}>
-        {dataPlans.map((item) => (
+    <>
+      <Container
+        sx={{
+          textAlign: "center",
+          minHeight: "85vh",
+          height: "fit-content",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "space-around",
+          justifyContent: "space-around",
+          marginTop: "2rem",
+          marginBottom: "2rem",
+        }}
+        className="text-primary"
+      >
+        <Typography variant="h3" sx={{ marginBottom: "1rem" }}>
+          What can I do for you <FontAwesomeIcon icon={faQuestionCircle} />
+        </Typography>
+        <Grid container gap={1} sx={{ justifyContent: "center" }}>
+          {dataPlans.map((item) => (
+            <Grid item xs={12} md={3}>
+              <Container
+                sx={{
+                  minHeight: "50vh",
+                  height: "fit-content",
+                  maxWidth: "50vw",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                  borderRadius: "9px",
+                  border: "3px",
+                }}
+                className="shadow-for-box container-background"
+              >
+                <Typography
+                  variant="h5"
+                  sx={{ fontFamily: "Lora", fontWeight: "500" }}
+                >
+                  {item.id}
+                </Typography>
+                <Typography>{item.description}</Typography>
+                <Box>
+                  {item.iconsText.map((text) => (
+                    <Box sx={{ display: "flex" }}>
+                      <Typography
+                        sx={{ fontFamily: "Lora", fontWeight: "500" }}
+                      >
+                        {text}
+                      </Typography>
+                    </Box>
+                  ))}
+                </Box>
+                <Link to={item.btnLink} style={{ textDecoration: "none" }}>
+                  <Button variant="contained" sx={{ margin: "1rem" }}>
+                    Samples
+                  </Button>
+                </Link>
+              </Container>
+            </Grid>
+          ))}
+
           <Grid item xs={12} md={3}>
             <Container
               sx={{
@@ -52,70 +96,32 @@ const Plans = () => {
                 variant="h5"
                 sx={{ fontFamily: "Lora", fontWeight: "500" }}
               >
-                {item.id}
+                Full stack website
               </Typography>
-              <Typography>{item.description}</Typography>
+              <Typography>
+                Dynamic website that reacts to user input, has its own API{" "}
+              </Typography>
               <Box>
-                {item.iconsText.map((text) => (
-                  <Box sx={{ display: "flex" }}>
-                    <Typography sx={{ fontFamily: "Lora", fontWeight: "500" }}>
-                      {text}
-                    </Typography>
-                  </Box>
-                ))}
+                <FontAwesomeIcon
+                  className="link-color"
+                  style={{ margin: "1rem" }}
+                  icon={faWindowClose}
+                  size="2x"
+                />
+                <FontAwesomeIcon
+                  className="link-color"
+                  style={{ margin: "1rem" }}
+                  icon={faBookReader}
+                  size="2x"
+                />
               </Box>
-              <Link to={item.btnLink} style={{ textDecoration: "none" }}>
-                <Button variant="contained" sx={{ margin: "1rem" }}>
-                  Samples
-                </Button>
-              </Link>
+              <Typography variant="h6">Still working on it</Typography>
             </Container>
           </Grid>
-        ))}
-
-        <Grid item xs={12} md={3}>
-          <Container
-            sx={{
-              minHeight: "50vh",
-              height: "fit-content",
-              maxWidth: "50vw",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "space-around",
-              borderRadius: "9px",
-              border: "3px",
-            }}
-            className="shadow-for-box container-background"
-          >
-            <Typography
-              variant="h5"
-              sx={{ fontFamily: "Lora", fontWeight: "500" }}
-            >
-              Full stack website
-            </Typography>
-            <Typography>
-              Dynamic website that reacts to user input, has its own API{" "}
-            </Typography>
-            <Box>
-              <FontAwesomeIcon
-                className="link-color"
-                style={{ margin: "1rem" }}
-                icon={faWindowClose}
-                size="2x"
-              />
-              <FontAwesomeIcon
-                className="link-color"
-                style={{ margin: "1rem" }}
-                icon={faBookReader}
-                size="2x"
-              />
-            </Box>
-            <Typography variant="h6">Still working on it</Typography>
-          </Container>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Contact />
+    </>
   );
 };
 
