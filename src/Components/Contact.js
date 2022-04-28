1ae7dd5627e68dfabd45a624a6fbdf34b1e18115
Mancefas <mantvydasliobe@gmail.com>
2022-04-28 18:53:27 +0300

    contact modal to different component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,46 +1,16 @@
-import React, { useState, useRef } from "react";
-import {
-  Container,
-  Box,
-  Button,
-  TextField,
-  Typography,
-  Modal,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Container, Box, Button, Typography } from "@mui/material";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import config from "../config.json";
 
-import emailjs from "@emailjs/browser";
+import ContactModal from "./ContactModal";
 
 const Contact = () => {
   const [open, setOpen] = useState(false);
-  const form = useRef();
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        config.CONTACT_SERVICE_ID,
-        config.CONTACT_TEMPLATE_ID,
-        form.current,
-        config.CONTACT_USER_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    setOpen(false);
-  };
 
   return (
     <Container
@@ -75,6 +45,7 @@ const Contact = () => {
             Form
           </Button>
         </Box>
+        <ContactModal open={open} close={handleClose} />
 
         <Box
           sx={{
@@ -99,59 +70,6 @@ const Contact = () => {
           </Button>
         </Box>
       </Box>
-
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        sx={{ marginTop: "15vh" }}
-      >
-        <form
-          ref={form}
-          onSubmit={sendEmail}
-          style={{ minWidth: "30vw", width: "fit-content", margin: "auto" }}
-          className="container-background"
-        >
-          <Container
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              padding: "1rem",
-              borderRadius: "9px",
-            }}
-          >
-            <Typography variant="h5" sx={{ fontFamily: "Lora" }}>
-              Contact me
-            </Typography>
-
-            <TextField
-              label={"Your Name"}
-              margin="normal"
-              name="user_name"
-              required
-            ></TextField>
-            <TextField
-              label={"Your email"}
-              margin="normal"
-              name="user_email"
-              type="email"
-              required
-            ></TextField>
-            <TextField
-              label={"Your question or suggestion "}
-              margin="normal"
-              multiline
-              name="message"
-              required
-            ></TextField>
-            <Button variant="contained" type="submit">
-              Send
-            </Button>
-          </Container>
-        </form>
-      </Modal>
     </Container>
   );
 };
