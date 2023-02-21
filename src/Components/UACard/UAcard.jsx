d61a05d923bc57d394e3f39adf3e643d03e81058
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-02-21 21:28:05 +0200

    Container component changed to div, CSS to css file, CSS changes

--- src/Components/UACard/UAcard.jsx
+++ src/Components/UACard/UAcard.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
 
-import { Container } from '@mui/material';
-
 import styles from './UAcard.module.css';
 
 export default function UAcard() {
   return (
-    <Container
-      className={styles.container}
-      sx={{
-        minWidth: '15vw',
-        width: 'fit-content',
-        minHeight: '5vh',
-        height: 'fit-content',
-        marginTop: '1.5rem',
-        borderRadius: '7px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
+    <div className={styles.container}>
       <div>
         <p className={styles.text}>I don&apos;t work</p>
       </div>
       <div>
         <p className={styles.text2}>with Russia</p>
       </div>
-    </Container>
+    </div>
   );
 }
